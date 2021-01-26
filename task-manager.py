f3a5bfa009066e56f707c3ef13c3b5f4f027dd92
alvov <a@lvovlife.ru>
2021-01-26 17:49:54 +0300

    Задание по проектированию планировщика (менеджжера задач)

--- task-manager.py
+++ task-manager.py
@@ -1,5 +1,31 @@
+"""
+
+"""
+
+from datetime import date
+from datetime import datetime
+
+
 class Task:
-    pass
+
+    def __init__(self, title, due=None, priority=None, description=""):
+        if not type(due) is int:
+            raise ValueError('Parameter Due must be unix timestamp')
+        now = datetime.now()
+
+
+        self.title = title
+        self.description = description
+        self.priority = priority
+        self.due = due
+        self.created =
 
 class TaskList(list):
     pass
+
+
+t = Task("test", "21 февраля 2026")
+print(t.created)
+print(t.due)
+
+
